552653f6e97d08eab91b6a53b0daf781d614e63d
himangshuishere <dehimangshu2020@gmail.com>
2022-04-15 10:30:14 +0530

    Fixatures and Features Added!
    Added stateful class in main.dart
    Created question.dart module for keeping the codebase clean.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 // void main(){
 //   runApp(FirstApp());
@@ -7,14 +8,24 @@ import 'package:flutter/material.dart';
 // Alternative block of the above code.
 void main() => runApp(FirstApp());
 
-class FirstApp extends StatelessWidget {
+class FirstApp extends StatefulWidget {
 
-  var questionIndex = 0;
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FirstAppState();
+  }
+}
 
-  void answerQuestion() {
-    questionIndex += 1;
+class _FirstAppState extends State<FirstApp>{
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
     // print('Answer chosen!');
-    print(questionIndex);
+    print(_questionIndex);
   }
 
 
@@ -26,13 +37,13 @@ class FirstApp extends StatelessWidget {
     ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Hello World")),
+        appBar: AppBar(title: const Text("Hello World")),
         body: Column(
           children: [
-            Text(questions[questionIndex]), // Text
-            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion), // RaisedButton
-            RaisedButton(child: Text('Answer 2'), onPressed: () => print("Answer 2 chosen!")), // RaisedButton
-            RaisedButton(child: Text('Answer 3'), onPressed: () {
+            Question(questions[_questionIndex]), // Question
+            RaisedButton(child: const Text('Answer 1'), onPressed: _answerQuestion), // RaisedButton
+            RaisedButton(child: const Text('Answer 2'), onPressed: () => print("Answer 2 chosen!")), // RaisedButton
+            RaisedButton(child: const Text('Answer 3'), onPressed: () {
               print("Answer 3 chosen!");
             }),
           ],

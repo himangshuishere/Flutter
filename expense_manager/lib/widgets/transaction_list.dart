c90b96b853dcbb1e45a94a13233febf330ecd524
himangshuishere <dehimangshu2020@gmail.com>
2022-06-01 22:39:15 +0530

    Refactors Done!
    Decentralised transactions list into individual transaction items in transaction_item.dart

--- expense_manager/lib/widgets/transaction_list.dart
+++ expense_manager/lib/widgets/transaction_list.dart
@@ -1,3 +1,4 @@
+import 'package:expense_manager/widgets/transaction_item.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_manager/models/transaction.dart';
 import 'package:intl/intl.dart';
@@ -33,37 +34,7 @@ class TransactionList extends StatelessWidget {
           })
         : ListView.builder(
             itemBuilder: (ctx, index) {
-              return Card(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                elevation: 5,
-                child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                            child: Text('Rs. ${transactions[index].amount}')),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: MediaQuery.of(context).size.width > 420
-                        ? FlatButton.icon(
-                            onPressed: () => deleteTx(transactions[index].id),
-                            icon: Icon(Icons.delete),
-                            label: Text("Delete"),
-                            textColor: Theme.of(context).errorColor,
-                          )
-                        : IconButton(
-                            onPressed: () => deleteTx(transactions[index].id),
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                          )),
-              );
+              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
             },
             itemCount: transactions.length,
           );

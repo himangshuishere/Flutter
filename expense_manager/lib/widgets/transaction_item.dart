40ebe04de7423ea4b618fa95718b66b0831b56aa
himangshuishere <dehimangshu2020@gmail.com>
2022-06-04 20:51:22 +0530

    Refactorings Done!
    Added unique key value pairs for the transaction_items.

--- expense_manager/lib/widgets/transaction_item.dart
+++ expense_manager/lib/widgets/transaction_item.dart
@@ -1,14 +1,36 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import 'package:expense_manager/models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({Key? key, required this.transaction, required this.deleteTx }) : super(key: key);
 
   final Transaction transaction;
   final Function deleteTx;
 
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color? _bgColor;
+  
+  @override
+  void initState(){
+    const availableColors = [
+      Colors.red,
+      Colors.indigo,
+      Colors.blue,
+      Colors.purple,
+
+    ];
+    _bgColor = availableColors[Random().nextInt(4)];
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -16,28 +38,29 @@ class TransactionItem extends StatelessWidget {
                 elevation: 5,
                 child: ListTile(
                     leading: CircleAvatar(
+                      backgroundColor: _bgColor,
                       radius: 30,
                       child: Padding(
                         padding: const EdgeInsets.all(6.0),
                         child: FittedBox(
-                            child: Text('Rs. ${transaction.amount}')),
+                            child: Text('Rs. ${widget.transaction.amount}')),
                       ),
                     ),
                     title: Text(
-                      transaction.title,
+                      widget.transaction.title,
                       style: Theme.of(context).textTheme.headline6,
                     ),
                     subtitle: Text(
-                        DateFormat.yMMMd().format(transaction.date)),
+                        DateFormat.yMMMd().format(widget.transaction.date)),
                     trailing: MediaQuery.of(context).size.width > 420
                         ? FlatButton.icon(
-                            onPressed: () => deleteTx(transaction.id),
+                            onPressed: () => widget.deleteTx(widget.transaction.id),
                             icon: Icon(Icons.delete),
                             label: Text("Delete"),
                             textColor: Theme.of(context).errorColor,
                           )
                         : IconButton(
-                            onPressed: () => deleteTx(transaction.id),
+                            onPressed: () => widget.deleteTx(widget.transaction.id),
                             icon: Icon(Icons.delete),
                             color: Theme.of(context).errorColor,
                           )),

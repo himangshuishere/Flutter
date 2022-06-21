adead2359abbf1d65cac2f332ffffc0aedfee47c
himangshuishere <dehimangshu2020@gmail.com>
2022-06-21 19:41:41 +0530

    Refactors Done!
    Finished working in the Orders Screen page.
    
    Added App Drawer widget.

--- shop_app/lib/screens/orders_screen.dart
+++ shop_app/lib/screens/orders_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/orders';
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +16,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),

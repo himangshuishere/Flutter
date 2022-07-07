1d2ad3d190b052426238e64ff301338e45f27327
himangshuishere <dehimangshu2020@gmail.com>
2022-07-07 11:17:06 +0530

    Started and Completed
    Finished the design of the login page.

--- notes_app/lib/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color black18_18_18 = Color.fromARGB(255, 18, 18, 18);
-const Color cyan_36_187_207 = Color.fromARGB(221, 36, 187, 207);
-const Color black30_30_30 = Color.fromARGB(255, 30, 30, 30);
